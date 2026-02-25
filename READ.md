04dcbe9df3a51b2459a622a3b9d1177cdf93c53c
Rashmika Thehan <ict22834@fot.sjp.ac.lk>
2026-02-26 01:08:51 +0530

    Initial commit

---
+++ READ.md
@@ -0,0 +1,5 @@
+# To test the WebSocket server, you need to install wscat using following command,
+    npm install -g wscat
+
+# you can use the following command in your terminal to connect to the WebSocket server:
+    ws -c ws://localhost:8080
---
+++ server.js
@@ -0,0 +1,32 @@
+import {WebSocketServer, WebSocket} from 'ws';
+
+//! 0: Connecting
+//! 1: Open (only state where you can use .send() )
+//! 2: Closing
+//! 3: Closed
+
+const wss = new WebSocketServer({port: 8080});
+
+//connection event
+wss.on("connection", (socket, request) =>{
+    const ip = request.socket.remoteAddress;
+
+    socket.on('message', (rawData) => {
+        const message = rawData.toString();
+        console.log({ rawData });
+
+        wss.clients.forEach((client) => {
+            if(client.readyState === WebSocket.OPEN) client.send(`Server Broadcast: ${message}`);
+        })
+    })
+
+    socket.on('error', (err)=>{
+        console.error(`Error: ${err.message}: ${ip}`);
+    })
+
+    socket.on('close', () => {
+        console.log(`Client disconnected: ${ip}`);
+    })
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
